9846c44690b8d785b8e6aee9bce6e0c532e85bd8
Jamie Pham <jtkp66@gmail.com>
2018-10-29 11:22:19 -0700

    added crete post component and form, post model and finished angular front end

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
- templateUrl: './post-create.component.html',
- styleUrls: ['./post-create.component.css']
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
-
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'No Content';
+  enteredTitle = '';
+  enteredContent = '';
 
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
